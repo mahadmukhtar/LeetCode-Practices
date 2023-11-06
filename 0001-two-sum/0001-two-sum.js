e088b30e001c580f0f67f0b9df6984a8c1e3d682
Mahad Bin Mukhtar <mahad.mukhtar@gmail.com>
2023-11-06 06:25:55 +0500

    Time: 136 ms (13.36%), Space: 42.2 MB (77.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const givenArray = JSON.parse(JSON.stringify(nums));
     for (let i = 0; i < nums.length; i++) {
         const firstNum = nums[i];
         nums[i] = null;
         let secondNumberIndex = nums.indexOf(target - firstNum);
-        if (firstNum + givenArray[secondNumberIndex] === target) {
+        if (firstNum + nums[secondNumberIndex] === target) {
             return [i, secondNumberIndex];
         }
     }
